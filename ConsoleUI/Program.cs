609ede98cc9795c7eac9e017627ba6bb60b88392
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-27 00:13:24 -0600

    demo complete.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace ConsoleUI;
+﻿using CoreLibrary; // core is actually using standard library
+//using FrameworkLibrary;
+
+namespace ConsoleUI;
 
 public class Program
 {
     static void Main(string[] args)
     {
+        Generators generators = new Generators();
+        PersonModel person = new PersonModel { Prefix = "Mr.", FirstName = "Mohammad", LastName = "Jabir" };
 
+        string message = generators.WelcomeMessage("Mr.", "Jabir");
+        Console.WriteLine(message);
 
         Console.ReadLine();
     }
---
+++ FrameworkLibrary/Generators.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkLibrary
+{
+    public class Generators
+    {
+        public string WelcomeMessage(string prefix, string lastName)
+        {
+            return $"Welcome to our demo application {prefix} {lastName}.";
+        }
+
+        public string FarewellMessage(string prefix, string lastName)
+        {
+            return $"I hope you enjoyed your time with us {prefix} {lastName}. Please come back soon.";
+        }
+    }
+}
