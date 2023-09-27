8a0991b392e9d940cca0e6fe682db3c5c81852b9
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-26 23:57:35 -0600

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleUI;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        Console.ReadLine();
+    }
+}
---
+++ FrameworkLibrary/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkLibrary
+{
+    public class PersonModel
+    {
+        public string Prefix { get; set; } // Mr., Mrs., Miss, Dr., etc.
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
